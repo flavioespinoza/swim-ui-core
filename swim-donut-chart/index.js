c9512b13263ad735784220fb936411510bb27fa9
flavioespinoza <flavio.espinoza@gmail.com>
2017-04-25 16:04:31 -0700

    Added color styling to donut chart.

--- swim-donut-chart/index.js
+++ swim-donut-chart/index.js
@@ -7,43 +7,32 @@ tag('x-swim-donut-chart', {
 
         var _self = this;
         _self.data = _self.getData();
-        var sum = [];
-        for (var i = 0; i < _self.data.length; i++) {
-            sum.push(_self.data[i][1]);
-        }
-        var title = 'Total: ' + _.sum(sum);
+        var title = _self.data[0][1] + '%';
 
         _self._chart = c3.generate({
             bindto: $('.chart', _self)[0],
             data: {
                 columns: _self.data,
                 type : 'donut',
+                colors: {
+                    Uptime: '#709ed4',
+                    Downtime: '#DDDDDD'
+                },
                 onclick: function (d, i) { console.log("onclick", d, i); },
-                onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-                onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+                onmouseover: function (d, i) {  },
+                onmouseout: function (d, i) {  }
             },
             donut: {
                 title: title
             },
-            axis: {
-                x: {
-                    show: false
-                },
-                y: {
-                    show: false
-                }
-            },
-            point: {
-                show: false
-            },
             legend: {
-                show: false
+                show: true
             }
         });
     },
     methods: {
         getData: function () {
-            return [['R0001', 30], ['R1002', 120]]
+            return [['Uptime', 93], ['Downtime', 7]]
         }
     }
 

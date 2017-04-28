012e809a6aa862d5f83bb5b3bb6104fd3390c8e7
flavioespinoza <flavio.espinoza@gmail.com>
2017-04-27 19:13:34 -0700

    Changed C3 line chart to pure D3.

--- swim-line-chart/index.js
+++ swim-line-chart/index.js
@@ -4,34 +4,103 @@ var c3 = require('c3');
 tag('x-swim-line-chart', {
     template: require('./template.html'),
     inserted: function () {
-        this._chart = c3.generate({
-            bindto: $('.chart', this)[0],
-            data: this._data || {columns:[]},
-            axis: {
-                x: {
-                    show: this._axis_x
-                },
-                y: {
-                    show: this._axis_y
-                }
+
+
+        //TODO: Derek - Need to feed in interval
+        var duration = 500;
+        var limit = 50;
+        var now = new Date(Date.now() - duration);
+
+        var width = this.clientWidth;
+        var height = this.clientHeight;
+
+        var groups = {
+            max: {
+                color: 'orange',
+                data: d3.range(limit).map(function () {
+                    return 0
+                })
             },
-            point: {
-                show: this._point
+            mean: {
+                color: 'green',
+                data: d3.range(limit).map(function () {
+                    return 0
+                })
             },
-            legend: {
-                show: this._legend
-            },
-            tooltip: {
-                show: this._tooltip
-            },
-            transition: {
-                duration: 150
+            min: {
+                color: 'blue',
+                data: d3.range(limit).map(function () {
+                    return 0
+                })
             }
-        });
+        };
+
+        var x = d3.time.scale()
+            .domain([now - (limit - 2), now - duration])
+            .range([0, width]);
+
+        var y = d3.scale.linear()
+            .domain([0, 100])
+            .range([height, 0]);
+
+        var line = d3.svg.line()
+            .interpolate('basis')
+            .x(function (d, i) {
+                return x(now - (limit - 1 - i) * duration)
+            })
+            .y(function (d) {
+                return y(d)
+            });
+
+        var svg = d3.select(this).select('.chart').append('svg')
+            .attr('width', width)
+            .attr('height', height + 50);
+
+        var paths = svg.append('g');
+
+        for (var name in groups) {
+            var group = groups[name];
+            group.path = paths.append('path')
+                .data([group.data])
+                .attr('class', name + ' group')
+                .style('stroke', group.color)
+                .style('stroke-width', 3)
+                .style('fill', 'none');
+        }
+
+        this._chart = function (data) {
+
+            now = new Date();
+
+            // Add new values
+            for (var name in groups) {
+                var group = groups[name];
+                group.data.push(data[name].value);
+                group.path.attr('d', line);
+            }
+
+            // Shift domain
+            x.domain([now - (limit - 2) * duration, now - duration]);
+
+            // Slide paths left
+            paths.attr('transform', null)
+                .transition()
+                .duration(duration)
+                .ease('linear')
+                .attr('transform', 'translate(' + x(now - (limit - 1) * duration) + ')')
+                .each('end', this._chart);
+
+            // Remove oldest data point from each group
+            for (var name in groups) {
+                var group = groups[name];
+                group.data.shift()
+            }
+        };
+
     },
     methods: {
-        load: function(data) {
-            this._chart.load(data);
+        load: function (obj) {
+            this._chart(obj)
         }
     },
     accessors: {
@@ -40,57 +109,57 @@ tag('x-swim-line-chart', {
         // to the `limited-count` attribute.
         data: {
             attribute: {},
-            get: function() {
+            get: function () {
                 return this._data;
             },
-            set: function(value) {
+            set: function (value) {
                 this._data = JSON.parse(value);
             }
         },
         tooltip: {
-          attribute: {boolean: true},
-          get: function(){
-            return this._tooltip;
-          },
-          set: function(value){
-            this._tooltip = value;
-          }
+            attribute: {boolean: true},
+            get: function () {
+                return this._tooltip;
+            },
+            set: function (value) {
+                this._tooltip = value;
+            }
         },
         legend: {
-          attribute: {boolean: true},
-          get: function(){
-            return this._legend;
-          },
-          set: function(value){
-            this._legend = value;
-          }
+            attribute: {boolean: true},
+            get: function () {
+                return this._legend;
+            },
+            set: function (value) {
+                this._legend = value;
+            }
         },
         point: {
-          attribute: {boolean: true},
-          get: function(){
-            return this._point;
-          },
-          set: function(value){
-            this._point = value;
-          }
+            attribute: {boolean: true},
+            get: function () {
+                return this._point;
+            },
+            set: function (value) {
+                this._point = value;
+            }
         },
         axis_x: {
-          attribute: {boolean: true},
-          get: function(){
-            return this._axis_x;
-          },
-          set: function(value){
-            this._axis_x = value;
-          }
+            attribute: {boolean: true},
+            get: function () {
+                return this._axis_x;
+            },
+            set: function (value) {
+                this._axis_x = value;
+            }
         },
         axis_y: {
-          attribute: {boolean: true},
-          get: function(){
-            return this._axis_y;
-          },
-          set: function(value){
-            this._axis_y = value;
-          }
+            attribute: {boolean: true},
+            get: function () {
+                return this._axis_y;
+            },
+            set: function (value) {
+                this._axis_y = value;
+            }
         }
     }
 });
